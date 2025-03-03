dcff95e618b465938e602ca49648a39bd1073dc1
Aldeva Rizky <aldevarwpp@gmail.com>
2025-03-04 03:11:37 +0700

    feat(cors): update CORS configuration for production and development

--- config/cors.php
+++ config/cors.php
@@ -19,21 +19,16 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
-        'http://localhost:5173',
-        'http://localhost:8000',
-        'http://127.0.0.1:8000',
-        'http://127.0.0.1:5173',
-        'http://api-lmsystem.euphemia.site',
-    ],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
 
-    'exposed_headers' => ['Authorization', 'X-Custom-Header'],
+    'exposed_headers' => [],
 
     'max_age' => 0,
 
     'supports_credentials' => true,
+
 ];
